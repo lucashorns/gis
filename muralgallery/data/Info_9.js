7aae5f8bd9e48093ab7a2daee263504606a4b8d6
lucashorns <98054419+lucashorns@users.noreply.github.com>
2023-06-27 16:40:30 -0600

    Add files via upload

---
+++ muralgallery/data/Info_9.js
@@ -0,0 +1,1 @@
+var json_Info_9 = {"type":"FeatureCollection","name":"Info_9","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":null},"geometry":{"type":"Point","coordinates":[-111.893740113166544,40.722485409632647]}}]}
